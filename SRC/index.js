055ed54f41e43dfad8abf59083e6c3b8235c1130
Blad <65477009+blad232@users.noreply.github.com>
2024-04-03 10:16:24 -0400

    Responsiveness

--- SRC/index.js
+++ SRC/index.js
@@ -5,11 +5,23 @@ const svgShare = document.getElementById("svg");
 const creatorCard = document.querySelector(".creator__card");
 const cardContact = document.querySelector(".card__contact");
 const share = document.querySelector(".share");
+const triangle = document.querySelector(".triangle");
+let x = window.matchMedia("(min-width:700px)");
+
 iconShare.addEventListener("click", () => {
-    iconShare.classList.toggle("--background-share");
-    creatorCard.classList.toggle("--background-card-contact");
-    cardContact.classList.toggle("hidden");
-    share.classList.toggle("hidden");
+    if (window.innerWidth < 700) {
+        iconShare.classList.toggle("--background-share");
+        creatorCard.classList.toggle("--background-card-contact");
+        cardContact.classList.toggle("hidden");
+        share.classList.toggle("hidden");
+
+    } else if (window.innerWidth > 700) {
+        share.classList.toggle("hidden");
+        iconShare.classList.toggle("--background-share");
+        share.classList.toggle("--background-card-contact");
+        triangle.classList.toggle("hidden");
+
+    }
 
 
 })
